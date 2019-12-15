2ede3c6723401ec8a2de769628843b9e5d42c4e9
Szabó Balázs <y051597@hu.generali.com>
2019-12-15 20:46:31 +0100

    nincs komment

---
+++ 03 - Python Objects and Data Type Basics/014 - Variable Assignments.py
@@ -0,0 +1,17 @@
+my_dogs = 2
+my_dogs = ["Sammy", "Franky"]
+a = 5
+print(a)
+a = 10
+print(a)
+print(a + a)
+a = a + a
+print(a)
+print(type(a))
+a = 1.2
+print(type(a))
+
+my_income = 100
+tax_rate = 0.1
+my_taxes = my_income*tax_rate
+print(my_taxes)
---
+++ 03 - Python Objects and Data Type Basics/015 - Introduction to strings.py
@@ -0,0 +1,11 @@
+# hello
+# 0,1,2,3,4 - Indexing
+# 0, -4, -3, -2, -1 - Reverse indexing
+# Slicing: [start:stop:step]
+
+print('hello')
+print("world")
+print('this is also a string')
+print("I'm going on a run")
+print('hello world')
+print('hello \n world')
