cda3779b1786aad3e54614c27ee5c987f3a5edd3
SzBalazs <szabo.balazs13@gmail.com>
2020-01-18 16:35:13 +0100

    Folytatás

---
+++ 04 - Python Comparison Operators/033 - Chaining Comparison Operators in Python with Logical Operators.py
@@ -0,0 +1,12 @@
+print(1 < 2 < 3)
+print(1 < 2 > 3)
+print(1 < 2 and 2 < 3)
+print('h' == 'h' and 2 == 2)
+print(('h' == 'h') and (2 == 2)) # csak szebben néz ki...
+
+print(1 == 1 or 2 == 2)
+print(100 == 1 or 2 == 2)
+
+print(not(1 == 1))
+print(not (400 > 500))
+print (1 != 1)
